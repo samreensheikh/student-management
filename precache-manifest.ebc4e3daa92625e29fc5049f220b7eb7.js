4230994378693c961c8360ce25a7941d02fe0094
samrin sheikh <samreensheikh387@gmail.com>
2020-08-16 14:31:09 +0530

    Updates

---
+++ precache-manifest.ebc4e3daa92625e29fc5049f220b7eb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee15e1491fd767b5c37c1774f5556d24",
+    "url": "/student-management/index.html"
+  },
+  {
+    "revision": "f51120455e89ff4603cd",
+    "url": "/student-management/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "624f4c0cfc85d122eb43",
+    "url": "/student-management/static/css/main.54f74775.chunk.css"
+  },
+  {
+    "revision": "f51120455e89ff4603cd",
+    "url": "/student-management/static/js/2.6a32c7cb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/student-management/static/js/2.6a32c7cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "624f4c0cfc85d122eb43",
+    "url": "/student-management/static/js/main.3b27da73.chunk.js"
+  },
+  {
+    "revision": "bfdf76455a5f870de84f",
+    "url": "/student-management/static/js/runtime-main.0bc48b10.js"
+  }
+]);
